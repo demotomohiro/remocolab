fd67faa141a594d5709b419555a24670c922930f
Rayan fernandes <37145078+Rayanfer32@users.noreply.github.com>
2019-12-05 12:34:52 +0530

    Delete setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-    name = "remocolab.py",
-    version = "0.1",
-    py_modules = ['remocolab'],
-    url = "https://github.com/demotomohiro/remocolab",
-    author = "demotomohiro",
-)
