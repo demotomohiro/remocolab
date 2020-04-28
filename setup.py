9a3b616ae424af6a5d530f74f73f70bf590b37ca
fhforest <60838792+cdkfox@users.noreply.github.com>
2020-04-28 09:06:03 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
     name = "remocolab.py",
     version = "0.1",
     py_modules = ['remocolab'],
-    url = "https://github.com/demotomohiro/remocolab",
-    author = "demotomohiro",
+    url = "https://github.com/cdkfox/remocolab",
+    author = "cdkfox",
 )
