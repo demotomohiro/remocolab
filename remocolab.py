a1d676fe08dfa0ec1234838fd71b291b88cef26e
demotomohiro <gpuppur@gmail.com>
2020-12-29 21:23:54 +0900

    Make remocolab runs on kaggle by not using apt module

--- remocolab.py
+++ remocolab.py
@@ -1,81 +1,22 @@
-import apt, apt.debfile
-import pathlib, stat, shutil, urllib.request, subprocess, getpass, time, tempfile
+import pathlib, stat, shutil, urllib.request, subprocess, getpass, time, tempfile, os
 import secrets, json, re
 import IPython.utils.io
 import ipywidgets
 import pyngrok.ngrok, pyngrok.conf
 
-# https://salsa.debian.org/apt-team/python-apt
-# https://apt-team.pages.debian.net/python-apt/library/index.html
-class _NoteProgress(apt.progress.base.InstallProgress, apt.progress.base.AcquireProgress, apt.progress.base.OpProgress):
-  def __init__(self):
-    apt.progress.base.InstallProgress.__init__(self)
-    self._label = ipywidgets.Label()
-    display(self._label)
-    self._float_progress = ipywidgets.FloatProgress(min = 0.0, max = 1.0, layout = {'border':'1px solid #118800'})
-    display(self._float_progress)
+def _installPkg(name):
+  print(f"Install {name}")
+  #Without DEBIAN_FRONTEND=noninteractive, apt-get might call a program that wait for user input.
+  newEnv = os.environ.copy()
+  newEnv["DEBIAN_FRONTEND"] = "noninteractive"
+  subprocess.run(["apt-get", "-q", "-y", "install", name], check = True, env = newEnv)
 
-  def close(self):
-    self._float_progress.close()
-    self._label.close()
+def _installPkgs(*args):
+  for i in args:
+    _installPkg(i)
 
-  def fetch(self, item):
-    self._label.value = "fetch: " + item.shortdesc
-
-  def pulse(self, owner):
-    self._float_progress.value = self.current_items / self.total_items
-    return True
-
-  def status_change(self, pkg, percent, status):
-    self._label.value = "%s: %s" % (pkg, status)
-    self._float_progress.value = percent / 100.0
-
-  def update(self, percent=None):
-    self._float_progress.value = self.percent / 100.0
-    self._label.value = self.op + ": " + self.subop
-
-  def done(self, item=None):
-    pass
-
-class _MyApt:
-  def __init__(self):
-    self._progress = _NoteProgress()
-    self._cache = apt.Cache(self._progress)
-
-  def close(self):
-    self._cache.close()
-    self._cache = None
-    self._progress.close()
-    self._progress = None
-
-  def update_upgrade(self):
-    self._cache.update()
-    self._cache.open(None)
-    self._cache.upgrade()
-
-  def commit(self):
-    self._cache.commit(self._progress, self._progress)
-    self._cache.clear()
-
-  def installPkg(self, *args):
-    for name in args:
-      pkg = self._cache[name]
-      if pkg.is_installed:
-        print(f"{name} is already installed")
-      else:
-        print(f"Install {name}")
-        pkg.mark_install()
-
-  def installDebPackage(self, name):
-    apt.debfile.DebPackage(name, self._cache).install()
-
-  def deleteInstalledPkg(self, *args):
-    for pkg in self._cache:
-      if pkg.is_installed:
-        for name in args:
-          if pkg.name.startswith(name):
-            #print(f"Delete {pkg.name}")
-            pkg.mark_delete()
+def _installDeb(path):
+  subprocess.run(["dpkg", "-i", str(path)], check = True)
 
 def _download(url, path):
   try:
@@ -117,24 +58,15 @@ def _set_public_key(user, public_key):
       shutil.chown(auth_keys_file, user)
 
 def _setupSSHDImpl(public_key, tunnel, ngrok_token, ngrok_region, mount_gdrive_to, mount_gdrive_from, is_VNC):
-  #apt-get update
-  #apt-get upgrade
-  my_apt = _MyApt()
-  #Following packages are useless because nvidia kernel modules are already loaded and I cannot remove or update it.
-  #Uninstall them because upgrading them take long time.
-  my_apt.deleteInstalledPkg("nvidia-dkms", "nvidia-kernel-common", "nvidia-kernel-source")
-  my_apt.commit()
-  my_apt.update_upgrade()
-  my_apt.commit()
+  subprocess.run(["apt-get", "-q", "update"], check = True)
+  subprocess.run(["apt-get", "-q", "-y", "upgrade"], check = True)
 
-  subprocess.run(["unminimize"], input = "y\n", check = True, universal_newlines = True)
+  if shutil.which("unminimize"):
+    subprocess.run(["unminimize"], input = "y\n", check = True, universal_newlines = True)
 
-  my_apt.installPkg("openssh-server")
+  _installPkg("openssh-server")
   if mount_gdrive_to:
-    my_apt.installPkg("bindfs")
-
-  my_apt.commit()
-  my_apt.close()
+    _installPkg("bindfs")
 
   #Reset host keys
   for i in pathlib.Path("/etc/ssh").glob("ssh_host_*_key"):
@@ -359,14 +291,13 @@ def _setupVNC():
   _download(libjpeg_url, "libjpeg-turbo.deb")
   _download(virtualGL_url, "virtualgl.deb")
   _download(turboVNC_url, "turbovnc.deb")
-  my_apt = _MyApt()
-  my_apt.installDebPackage("libjpeg-turbo.deb")
-  my_apt.installDebPackage("virtualgl.deb")
-  my_apt.installDebPackage("turbovnc.deb")
+  _installDeb("libjpeg-turbo.deb")
+  _installDeb("virtualgl.deb")
+  _installDeb("turbovnc.deb")
 
-  my_apt.installPkg("xfce4", "xfce4-terminal")
-  my_apt.commit()
-  my_apt.close()
+  #modprobe is required by nvidia driver installer but kaggle doesn't have it.
+  #kmod provides modprobe.
+  _installPkgs("xfce4", "xfce4-terminal", "kmod")
 
   vnc_sec_conf_p = pathlib.Path("/etc/turbovncserver-security.conf")
   vnc_sec_conf_p.write_text("""\
